c0f197186d7f6948b4de55cf07da4e1190a50b90
Iain R. Learmonth <irl@fsfe.org>
2015-04-22 20:46:31 +0100

    Refactoring import script

--- update-from-udd.py
+++ update-from-udd.py
@@ -26,7 +26,7 @@ from semantic_debian.namespaces import SRCPKG
 from semantic_debian.namespaces import SCREENSHOTS
 from semantic_debian.namespaces import DDPO
 
-# Helpful things
+## Helpful things
 rfc822_extract = re.compile('^(.*) <(.*)>$')
 
 repotype = {
@@ -40,20 +40,30 @@ repotype = {
         'Darcs': DOAP.DarcsRepository,
         }
 
-# Setup an RDFLib graph
+## Setup an RDFLib graph
 
 g = Graph()
 g.namespace_manager = namespace_manager
 
-# Generate Debian triples
+## We need to store maintainer, uploader and translator details for later
+
+maintainers = []
+
+## Generate Debian triples
+
+# TODO: There should be translations for the description here.
 
 debian = URIRef('http://rdf.debian.net/debian')
 g.add( (debian, RDF.type, ADMSSW.SoftwareProject) )
 g.add( (debian, DOAP.name, Literal('The Debian Project')) )
-g.add( (debian, DOAP.description, Literal('Debian is a free operating system (OS) for your computer. An operating system is the set of basic programs and utilities that make your computer run. Debian provides more than a pure OS: it comes with over 37500 packages, precompiled software bundled up in a nice format for easy installation on your machine.', lang="en")) )
+g.add( (debian, DOAP.description, Literal('Debian is a free operating system ' +
+   '(OS) for your computer. An operating system is the set of basic programs ' +
+   'and utilities that make your computer run. Debian provides more than a ' +
+   'pure OS: it comes with over 37500 packages, precompiled software bundled ' +
+   'up in a nice format for easy installation on your machine.', lang="en")) )
 g.add( (debian, DOAP.homepage, URIRef('http://www.debian.org/')) )
 
-# Generate all release triples
+## Generate all release triples
 
 releases = [x[0] for x in udd.query("SELECT DISTINCT release FROM sources")]
 
@@ -69,81 +79,93 @@ for release in releases:
     g.add( (RELEASE[release], RDF.type, ADMSSW.SoftwareRelease) )
     g.add( (RELEASE[release], DOAP.name, Literal("Debian " + release)) )
 
-# Generate all project triples
+## Generate all project triples
 
-packages = udd.query("SELECT source, version, release, maintainer_name, maintainer_email, uploaders, vcs_type, vcs_url, vcs_browser FROM sources WHERE section='hamradio'")
+projects = [{'source': x[0],
+             'maintainer_name': x[1],
+             'maintainer_email': x[2],
+             'uploaders': x[3],}
+             for x in udd.query("SELECT source, maintainer_name, " +
+                                "maintainer_email, uploaders FROM sources")]
 
-for package in packages:
-    # Break out uploaders
-    uploaders = []
-    if package[5] != None:
-        for u in package[5].split(', '):
+for p in projects:
+    # Basic project triples
+    g.add( (PROJECT[p['source']], RDF.type, ADMSSW.SoftwareProject) )
+    g.add( (PROJECT[p['source']], DOAP.name, Literal("Debian " + p['source'] + " packaging")) )
+    g.add( (PROJECT[p['source']], DOAP.shortdesc, Literal("Maintenance of the " + p['source'] + " source package in Debian", lang="en")) )
+    g.add( (PROJECT[p['source']], DOAP.homepage, TRACKER[p['source']]) )
+    g.add( (PROJECT[p['source']], DOAP['download-page'], SRCPKG[p['source']]) )
+    g.add( (PROJECT[p['source']], DOAP.screenshots, SCREENSHOTS[p['source']]) )
+    g.add( (PROJECT[p['source']], DOAP.platform, Literal('dpkg')) )
+
+    # Add maintainer
+    g.add( (PROJECT[p['source']], DOAP.maintainer, 
+        MAINTAINER[quote(p['maintainer_email'])]) )
+    maintainers.append( (p['maintainer_name'], p['maintainer_email'],) )
+
+    # Add uploaders
+    if p['uploaders'] != None:
+        for u in p['uploaders'].split(', '):
             matches = rfc822_extract.match(u)
             if matches != None:
-                uploaders.append(matches.group(1,2))
-   
-    g.add( (PROJECT[package[0]], RDF.type, ADMSSW.SoftwareProject) )
-    g.add( (PROJECT[package[0]], DOAP.name, Literal("Debian " + package[0] + " packaging")) )
-    g.add( (PROJECT[package[0]], DOAP.description, Literal("Maintenance of the " + package[0] + " source package in Debian", lang="en")) )
+                u = matches.group(1,2)
+                g.add( (PROJECT[p['source']], DOAP.developer, MAINTAINER[quote(u[1])]) )
+                maintainers.append(u)
 
-    g.add( (PROJECT[package[0]], DOAP.homepage, TRACKER[package[0]]) )
+## Generate all maintainer triples
 
-    g.add( (PROJECT[package[0]], DOAP['download-page'], SRCPKG[package[0]]) )
+for maintainer in maintainers:
+    name = maintainer[0]
+    email = maintainer[1]
+    g.add( (MAINTAINER[quote(email)], RDF.type, FOAF.Agent) )
+    g.add( (MAINTAINER[quote(email)], FOAF.name, Literal(name)) )
+    g.add( (MAINTAINER[quote(email)], FOAF.mbox, URIRef('mailto:' + email)) )
+    g.add( (MAINTAINER[quote(email)], FOAF.mbox_sha1sum, Literal(sha1('mailto:' + email).hexdigest())) )
+    g.add( (MAINTAINER[quote(email)], FOAF.homepage, DDPO[quote(email)]) )
 
-    g.add( (PROJECT[package[0]], DOAP.release, PACKAGE[package[0] + "_" + package[1]]) )
-    g.add( (PACKAGE[package[0] + "_" + package[1]], ADMSSW.project, PROJECT[package[0]]) )
+## Generate all repository triples
 
-    vcs_type = package[6]
-    vcs_url = package[7].split(" ")[0] if package[7] != None else None
-    vcs_browser = package[8]
+# TODO: This should really use the vcswatch table instead.
+# TODO: Add git branch information
 
-    if vcs_type != None and vcs_type != "Cvs":
-        repository = URIRef('http://rdf.debian.net/repository/' + package[0])
-        g.add( (repository, RDF.type, repotype[vcs_type]) )
-        if vcs_url != None:
-            if " " in vcs_url or "<" in vcs_url or ">" in vcs_url or "{" in vcs_url:
-                print "W: VCS URL for %s (%s) contains a bad character, ignoring this URL" % (package[0], vcs_url,)
+repositories = [{'source': x[0],
+                 'vcs_type': x[1],
+                 'vcs_url': x[2],
+                 'vcs_browser': x[3],}
+                for x in udd.query("SELECT source, vcs_type, vcs_url, " +
+                    "vcs_browser FROM sources WHERE release='sid'")]
+
+for r in repositories:
+    # Git things might have branch information included
+    r['vcs_url'] = r['vcs_url'].split(" ")[0] if r['vcs_url'] != None else None
+
+    if r['vcs_type'] != None and r['vcs_type'] != "Cvs":
+        repository = URIRef('http://rdf.debian.net/repository/' + r['source'])
+        g.add( (repository, RDF.type, repotype[r['vcs_type']]) )
+        if r['vcs_url'] != None:
+            if " " in r['vcs_url'] or "<" in r['vcs_url'] or ">" in r['vcs_url'] or "{" in r['vcs_url']:
+                print "W: VCS URL for %s (%s) contains a bad character, ignoring this URL" % (r['source'], r['vcs_url'],)
             else:
-                g.add( (repository, DOAP['anon-root'], URIRef(vcs_url)) )
-        if vcs_browser != None:
-            if " " in vcs_browser or "<" in vcs_browser or ">" in vcs_browser or "{" in vcs_browser:
-                print "W: VCS Browser URL for %s (%s) contains a bad character, ignoring this URL" % (package[0], vcs_browser,)
+                g.add( (repository, DOAP['anon-root'], URIRef(r['vcs_url'])) )
+        if r['vcs_browser'] != None:
+            if " " in r['vcs_browser'] or "<" in r['vcs_browser'] or ">" in r['vcs_browser'] or "{" in r['vcs_browser']:
+                print "W: VCS Browser URL for %s (%s) contains a bad character, ignoring this URL" % (r['source'], r['vcs_browser'],)
             else:
-                g.add( (repository, DOAP.browse, URIRef(vcs_browser)) )
-        g.add( (PROJECT[package[0]], DOAP.repository, repository ) )
+                g.add( (repository, DOAP.browse, URIRef(r['vcs_browser'])) )
+        g.add( (PROJECT[r['source']], DOAP.repository, repository ) )
 
-    g.add( (RELEASE[package[2]], ADMSSW.includedAsset, PACKAGE[package[0] + "_" + package[1]]) )
-    g.add( (PACKAGE[package[0] + "_" + package[1]], RDF.type, ADMSSW.SoftwareRelease) )
-    g.add( (PACKAGE[package[0] + "_" + package[1]], DCTERMS.isPartOf, RELEASE[package[2]]) )
-    g.add( (PACKAGE[package[0] + "_" + package[1]], DOAP.revision, Literal(package[1])) )
-    g.add( (PACKAGE[package[0] + "_" + package[1]], DOAP.platform, Literal('dpkg')) )
-    g.add( (PROJECT[package[0]], DOAP.platform, Literal('dpkg')) )
+## Generate all release triples
 
-    g.add( (PROJECT[package[0]], DOAP.screenshots, SCREENSHOTS[package[0]]) )
+#    g.add( (RELEASE[package[2]], ADMSSW.includedAsset, PACKAGE[package[0] + "_" + package[1]]) )
+#    g.add( (PACKAGE[package[0] + "_" + package[1]], RDF.type, ADMSSW.SoftwareRelease) )
+#    g.add( (PACKAGE[package[0] + "_" + package[1]], DCTERMS.isPartOf, RELEASE[package[2]]) )
+#    g.add( (PACKAGE[package[0] + "_" + package[1]], DOAP.revision, Literal(package[1])) )
+#    g.add( (PACKAGE[package[0] + "_" + package[1]], DOAP.platform, Literal('dpkg')) )
 
-    g.add( (PROJECT[package[0]], DOAP.maintainer, MAINTAINER[quote(package[4])]) )
-    g.add( (MAINTAINER[quote(package[4])], RDF.type, FOAF.Agent) )
-    g.add( (MAINTAINER[quote(package[4])], FOAF.name, Literal(package[3])) )
-    g.add( (MAINTAINER[quote(package[4])], FOAF.mbox, URIRef('mailto:' + package[4])) )
-    g.add( (MAINTAINER[quote(package[4])], FOAF.mbox_sha1sum, Literal(sha1('mailto:' + package[4]).hexdigest())) )
-    g.add( (MAINTAINER[quote(package[4])], FOAF.homepage, DDPO[quote(package[4])]) )
+## Generate all upstream triples
 
-    for uploader in uploaders:
-        g.add( (PROJECT[package[0]], DOAP.developer, MAINTAINER[quote(uploader[1])]) )
-        g.add( (MAINTAINER[quote(uploader[1])], RDF.type, FOAF.Agent) )
-        g.add( (MAINTAINER[quote(uploader[1])], FOAF.name, Literal(uploader[0])) )
-        g.add( (MAINTAINER[quote(uploader[1])], FOAF.mbox, URIRef('mailto:' + uploader[1])) )
-        g.add( (MAINTAINER[quote(uploader[1])], FOAF.mbox_sha1sum, Literal(sha1('mailto:' + uploader[1]).hexdigest())) )
-        g.add( (MAINTAINER[quote(uploader[1])], FOAF.homepage, DDPO[quote(uploader[1])]) )
-
-# Generate all release triples
-
-# Generate all maintainer triples
-
-# Generate all upstream triples
-
-# Write out the RDFLib graph as a Turtle dump
+## Write out the RDFLib graph as a Turtle dump
 g.serialize("semdeb.ttl", format='turtle')
 
-# Make an xz compressed version of the dump
+## Make an xz compressed version of the dump
 
